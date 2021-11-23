cce3a26b14ec8e394657222a5687d953067ac180
KMalif <khafidulmualif2019@gmail.com>
2021-11-23 16:43:31 +0700

    4. Finish

--- app/src/main/java/com/plugin/quisuts/utils/DisableNightmode.kt
+++ app/src/main/java/com/plugin/quisuts/utils/DisableNightmode.kt
@@ -1,4 +1,11 @@
 package com.plugin.quisuts.utils
 
-class DisableNightmode {
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+class DisableNightmode : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
 }
